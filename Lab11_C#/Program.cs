6d922438dc34058a8a61dd1fc97cea39e4e4a113
l-Mel-l <125726970+l-Mel-l@users.noreply.github.com>
2024-05-02 13:38:40 +0300

    Добавьте файлы проекта.

---
+++ Lab11_C#/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program{
+    // Создаем делегат
+    public delegate string DayDelegate();
+
+    static void Main(){
+        // Массив с названиями дней недели
+        string[] daysOfWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+        int dayIndex = 0; // начальный индекс массива - 0
+
+        // Создаем экземпляр делегата с помощью лямбда-выражения
+        DayDelegate day = () => {
+            // Возвращаем текущий день недели
+            string currentDay = daysOfWeek[dayIndex];
+
+            // Увеличиваем индекс на 1 или сбрасываем его на 0 (Понедельник), если достигли конца массива
+            dayIndex += 1;
+            if (dayIndex >= daysOfWeek.Length) { dayIndex = 0; }
+
+            // Возвращаем название следующего дня недели
+            return currentDay;
+        };
+
+        for(int i = 0; i<14; i++){
+            Console.WriteLine(day()); 
+            Console.WriteLine("-------------\n"); 
+        }
+    }
+}
